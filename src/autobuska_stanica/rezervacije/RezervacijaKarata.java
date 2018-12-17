bba2c4d4629774a22ef1ae8b02f188f696c7c696
Nikola Milikic <nikola.milikic@gmail.com>
2018-12-17 13:49:05 +0100

    Obrisana resenja i testovi.

--- src/autobuska_stanica/rezervacije/RezervacijaKarata.java
+++
@@ -1,9 +0,0 @@
-package autobuska_stanica.rezervacije;
-
-import autobuska_stanica.Polazak;
-
-public interface RezervacijaKarata {
-	
-	void unesiPolazak(Polazak polazakZaUnos) throws Exception;
-	boolean rezervisiKarte(String destinacija, int brojKarata);
-}
